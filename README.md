e54d1766609a91d39c6cdf4af3ff19eaec92d3e1
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2023-09-09 21:07:04 +0200

    Intial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# YouTube downloader with PyTube
+
+## WARNING
+
+Educational purposes only. Downloading YouTube videos is against YouTubes TOS and i do not advise you to download videos from YouTube
+
+You can use this code for whatever you want, you don't have to credit me :)
---
+++ main.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+def Download(link):
+    print("YouTube video is downloading...")
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+
+link = input("Enter the YouTube video URL: ")
+Download(link)
